c2dee1fc1622fae5cabde0e32a68097aedbf6f4c
ilhamelhamdi <ilhamabdillah123@gmail.com>
2022-07-10 00:14:32 +0700

    fix bugs

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:9000/api'
+const API_URL = 'https://canteen-elhamdi.herokuapp.com/api'
 const API_IMAGE_SERVER_URL = 'https://api.imgur.com/3/image'
 const API_IMAGE_SERVER_AUTHORIZATION = 'Client-ID c025ce5cb3fa7d5'
 const JWT_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI5OTkyNyIsImlhdCI6MTY1NzI5NTM3N30.ua7JF6GJiFqpejoBX60Db2r8tHwhUdnmBIKN3Hau4YY'
