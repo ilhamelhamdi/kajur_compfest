660fdc6a3250e65ddb15d1e73950f59ce5834542
ilhamelhamdi <ilhamabdillah123@gmail.com>
2022-07-11 00:27:58 +0700

    installation config

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,7 @@
-const API_URL = 'https://canteen-elhamdi.herokuapp.com/api'
+const API_URL = window.location.host + '/api' // Change this to 'http://localhost:9000/api' if run it in development
+
+
 const API_IMAGE_SERVER_URL = 'https://api.imgur.com/3/image'
 const API_IMAGE_SERVER_AUTHORIZATION = 'Client-ID c025ce5cb3fa7d5'
-const JWT_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI5OTkyNyIsImlhdCI6MTY1NzI5NTM3N30.ua7JF6GJiFqpejoBX60Db2r8tHwhUdnmBIKN3Hau4YY'
 
-export { API_URL, API_IMAGE_SERVER_URL, API_IMAGE_SERVER_AUTHORIZATION, JWT_TOKEN }
+export { API_URL, API_IMAGE_SERVER_URL, API_IMAGE_SERVER_AUTHORIZATION }
